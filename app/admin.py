e29f6ea6365159aed6a91470a1d1220f38e93d7e
VladimirBurash <vladimirburash@ktsstudio.ru>
2018-02-27 03:17:51 +0300

    init

---
+++ app/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+
+from app.models import ProductService, Company, Lease
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'link', 'description', 'phone', 'floor', 'housing', 'get_tags')
+    fields = ('name', 'link', 'description', 'phone', 'floor', 'housing','tags')
+    list_filter = ('floor',)
+
+
+    def get_tags(self, obj):
+        return ', '.join([_.title for _ in obj.tags.all()])
+
+    get_tags.short_description = 'Услуги и товары'
+
+
+
+class LeaseAdmin(admin.ModelAdmin):
+    list_display = ('photo', 'square', 'cost', 'floor', 'housing', 'function', 'show')
+    fields = ('photo', 'square', 'cost', 'floor', 'housing', 'function', 'show')
+    list_filter = ('floor', 'cost')
+
+
+class ProductServiceAdmin(admin.ModelAdmin):
+    list_display = ('photo', 'title', 'description', 'show', 'type')
+    fields = ('photo', 'title', 'description', 'show', 'type')
+    list_filter = ('type',)
+
+
+admin.site.register(ProductService, ProductServiceAdmin)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Lease, LeaseAdmin)
---
+++ app/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.db.models import Model
+
+TYPES = (
+    (0, 'Товар'),
+    (1, 'Услуга'),
+)
+
+
+class ProductService(Model):
+    class Meta:
+        verbose_name = 'Товар или услуга'
+        verbose_name_plural = 'Товар или услуги'
+
+    photo = models.ImageField(upload_to='images', default=None, verbose_name='Иконка')
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
+    show = models.BooleanField(default=False, verbose_name='Показывать?')
+    type = models.IntegerField(choices=TYPES, verbose_name='Тип', default=1)
+
+    def __str__(self):
+        return self.title
+
+
+class Company(Model):
+    class Meta:
+        verbose_name = 'арендатор'
+        verbose_name_plural = 'арендаторы'
+
+    name = models.CharField(max_length=255, default='default', verbose_name='Имя арендатора')
+    link = models.CharField(blank=True, null=True, max_length=255, verbose_name='Сайт')
+    description = models.TextField(verbose_name='Описание')
+    phone = models.CharField(max_length=255, verbose_name='Телефон')
+    floor = models.IntegerField(verbose_name='Этаж')
+    housing = models.IntegerField(blank=True, null=True, verbose_name='Корпус')
+    tags = models.ManyToManyField(ProductService, verbose_name='Товары и услуги')
+
+    def __str__(self):
+        return self.name
+
+
+class Lease(Model):
+    class Meta:
+        verbose_name = 'аренда'
+        verbose_name_plural = 'аренды'
+
+    photo = models.ImageField(upload_to='images', default=None, verbose_name='Фото')
+    square = models.FloatField(verbose_name='Площадь')
+    cost = models.IntegerField(verbose_name='Цена')
+    floor = models.IntegerField(verbose_name='Этаж')
+    housing = models.IntegerField(blank=True, null=True, verbose_name='Корпус')
+    function = models.TextField(verbose_name='Назначение')
+    show = models.BooleanField(default=False, verbose_name='Показывать?')
+
+    def __str__(self):
+        return '{}, {}, {}'.format(str(self.square), str(self.floor), str(self.cost))
