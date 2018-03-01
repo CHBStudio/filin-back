4a219bff08bd7630796ff01c139baed4c2f4bbd0
VladimirBurash <vladimirburash@ktsstudio.ru>
2018-03-02 02:09:45 +0300

    fix

--- filin/urls.py
+++ filin/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
-from app.views import LandingView, ProductView
+from app.views import LandingView, ProductView, ServiceView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
